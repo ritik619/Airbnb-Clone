f1d562cfb7a04ae8e030b778aa60f586f0d72a2e
ritik619 <ritiksaini619@gmail.com>
2021-09-14 16:47:43 +0530

    Added center button

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,18 +1,27 @@
 import React from "react";
-import cover from "./cover.jpg";
 import bannerStyles from "./bannerStyles";
 import { Button } from "@material-ui/core";
+import Navbar from "../NavBar";
+
 const Banner = () => {
-  const classes = bannerStyles();
-  return (
+	const classes = bannerStyles();
+	return (
     <div className={classes.banner}>
-    <div className={classes.alignCenter}>
-      <div>Not sure where to go? Perfect.</div>
-      <br/>
-      <Button className={classes.centerButton} variant="contained" color="default">I'm flexible</Button> 
-    </div>
-    </div>
-  );
+      <Navbar/>
+			<div className={classes.alignCenter}>
+				<h3>Not sure where to go? Perfect.</h3>
+				<br />
+				<Button
+					size="large"
+					className={classes.centerButton}
+					variant="contained"
+					color="default"
+				>
+					<h3 className={classes.buttonLabel}>I'm flexible</h3>
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
--- src/components/banner/bannerStyles.js
+++ src/components/banner/bannerStyles.js
@@ -2,19 +2,26 @@ import { makeStyles } from "@material-ui/core";
 import cover from "./cover.jpg";
 
 const Styles = makeStyles({
-  banner: {
-    backgroundImage: `url(${cover})`,
-    height:'520px',
-    position:"relative"
-  },
-  alignCenter:{
-      position:"absolute",
-      bottom:100,
-      left:'46%',
-      color:"white"
-  },
-  centerButton:{
-      textTransform:'none',
-  }
+	banner: {
+		backgroundImage: `url(${cover})`,
+		height: "520px",
+		position: "relative",
+	},
+	alignCenter: {
+		textAlign: "center",
+    color: "white",
+    paddingTop:'300px'
+	},
+	centerButton: {
+		textTransform: "none",
+		width: 205,
+		height: 64,
+		borderRadius: "50px",
+	},
+	buttonLabel: {
+		background: "-webkit-linear-gradient(90deg, #6F019C 0%, #C6017E 135.12%)",
+		"-webkit-background-clip": "text",
+		"-webkit-text-fill-color": "transparent",
+	},
 });
 export default Styles;
